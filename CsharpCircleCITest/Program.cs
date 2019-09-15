5cdeded757243d72420e9b3f746a51e57128af81
CrazyFilip13 <crazy.filip13@gmail.com>
2019-09-15 15:15:46 +0200

    Initial commit

---
+++ CsharpCircleCITest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpCircleCITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test for CircleCI");
+        }
+    }
+}
